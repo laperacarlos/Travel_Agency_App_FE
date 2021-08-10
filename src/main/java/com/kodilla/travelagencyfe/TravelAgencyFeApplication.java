eff9fba48e8e8e21dc3b84538b11a05cc1f0d9a3
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-08-10 14:08:55 +0200

    First commit - added files created wtihin new Vaadin project initialization.

---
+++ src/main/java/com/kodilla/travelagencyfe/TravelAgencyFeApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.travelagencyfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyFeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyFeApplication.class, args);
+    }
+
+}
